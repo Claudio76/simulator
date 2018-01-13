73df22819b52eb698bcf9df31b4c80012ed619ab
Claudio Frigeri <xfricla@bancoindustrial.com.ar>
2018-01-13 13:49:09 -0300

    Add json properties

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 new Vue({
     el: "#prestamos",
 
+    ready: function(){
+        this.$http.get("./src/config/properties.json").then(response =>{
+            var data = response.body;
+            console.log(response.body);
+        });
+    },
+
     data: {
-        textTest : "Texto de prueba para VueJs"
-    }
+        textTest : "Texto de prueba para VueJs",
+        tnaconvenio: 2
+    },
+
+    methods:{}
 });
