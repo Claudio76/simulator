40977b131e28e25efb0dacc257f9941389dc5cc8
Claudio Frigeri <xfricla@bancoindustrial.com.ar>
2018-03-03 22:58:28 -0300

    cambio en path librerias

--- src/app.js
+++ src/app.js
@@ -2,6 +2,35 @@ Vue.component('modal', {
     template: '#modal-template'
   });
 
+Vue.component('select2', {
+    props: ['options', 'value'],
+    template: '#select2-template',
+    mounted: function () {
+      var vm = this
+      $(this.$el)
+        .val(this.value)
+        // init select2
+        .select2({ data: this.options })
+        // emit event on change.
+        .on('change', function () {
+          vm.$emit('input', this.value)
+        })
+    },
+    watch: {
+      value: function (value) {
+        // update value
+        $(this.$el).val(value)
+      },
+      options: function (options) {
+        // update options
+        $(this.$el).select2({ data: options })
+      }
+    },
+    destroyed: function () {
+      $(this.$el).off().select2('destroy')
+    }
+  })
+
 new Vue({
     el: "#prestamos",
 
@@ -47,6 +76,7 @@ new Vue({
         capitalSolic: 1000, 
         sueldoBruto: 10000,
         plazo:2,
+        qtyCuotas:4,
 
         convenio: 0,
         outConvenio: 0,
